ce33e627bb4f63499d3dcf7c6c6da8be09e72de5
matimarecki <mati.marecki@gmail.com>
2016-01-20 18:25:00 +0100

    Create dist.cc

---
+++ zajecia_sty20/dist.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define REP(i,n)  for(int i=0;i<(int)(n);++i)
+#define FOR(i,b,n)  for(int i=b;i<(n);++i)
+#define ALL(c) (c).begin(),(c).end()
+#define SS size()
+#define CLR(a,v) memset((a),(v), sizeof a)
+#define ST first
+#define ND second
+template<typename T, typename U> inline void AMIN(T &x, U y) { if(y < x) x = y; }
+template<typename T, typename U> inline void AMAX(T &x, U y) { if(x < y) x = y; }
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef pair<int,int> pii;
+
+double dist (double ax, double ay, double bx, double by){
+	return sqrt((ax - bx) * (ax - bx) + (ay - by) * (ax - by));
+}
+
+int main(){
+	cout << dist (0,0,1,1);
+}
